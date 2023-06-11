37998d4418c6fe1e9436973844033b890df25d0e
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-11 15:55:31 +0300

    Fixed Class controller does not exiet

---
+++ restaurant_app/app/Http/Controllers/Hotel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Hotel extends Controller
+{
+    //
+    function index()
+    {
+        return "Resto";
+    }
+}
--- restaurant_app/routes/web.php
+++ restaurant_app/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Hotel;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get("/",[Hotel::class,'index']);
