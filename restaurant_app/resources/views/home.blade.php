7bd5b99a6c42f919e0249caefe62fa5304dcb8f1
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-20 15:32:59 +0300

    Added image in assets folder

--- restaurant_app/resources/views/home.blade.php
+++ restaurant_app/resources/views/home.blade.php
@@ -15,22 +15,28 @@
          <img src="{{  URL('storage/assets/resto11.jpg') }}"  width="100%" height="70%"  alt="image" id="firstImage" ></div><br><br>
         </div>
          <div class="row ms-2 bg-color-light">
-            <div class="col-8">
+            <div class="col-5">
          <img src="{{  URL('storage/assets/resto1.jpg') }}"  width="70%" height="80%"  alt="image" id="firstImage" ></div>
-         <div class="col-3"><h1 class="lead text-center">About Us</h1>
-            <p>We are the BiWaMi Restaurant that provides best services to its clients</p>
+         <div class="col-3 ps-0" ><h1 class="lead text-center">About Us</h1>
+            <p>The diner is open from 6 am to 10 pm --and free parking is available behind our location at 320 Lancaster Ave, Wayne, Pennsylvania </p>
+        </div>
+        <div class="col">
+            <h1 class="lead text-center">our Mission</h1>
+            <p>Our restaurant strives to become our customers’ favorite place to eat and drink.</p>
         </div>
         </div>
     </div>
     <div class="container">
-        <h1 class="lead text-center">our Mission
-
-        </h1>
-        <div class="ms-5">
-         
+        
+        <div class="row ms-2 bg-color-light">
+            <div class="col-4">
+         <img src="{{  URL('storage/assets/resto1.jpg') }}"  width="70%" height="80%"  alt="image" id="firstImage" ></div>
+         <div class="col-8 ps-0" ><h1 class="lead text-center">About Us</h1>
+            <p>The diner is open from 6 am to 10 pm --and free parking is available behind our location at 320 Lancaster Ave, Wayne, Pennsylvania </p>
         </div>
     </div>
   
 </body>
+C:\xampp\htdocs\Restaurant_Laravel_Project\restaurant_app\public\storage\assets\cake.webp
 </html>
 @stop
