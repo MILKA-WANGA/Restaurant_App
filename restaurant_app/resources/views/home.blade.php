46a611c975e7ba333c6c120fb2e0b0276c0af0fb
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-20 15:43:52 +0300

    Added a paragraph

--- restaurant_app/resources/views/home.blade.php
+++ restaurant_app/resources/views/home.blade.php
@@ -19,24 +19,33 @@
          <img src="{{  URL('storage/assets/resto1.jpg') }}"  width="70%" height="80%"  alt="image" id="firstImage" ></div>
          <div class="col-3 ps-0" ><h1 class="lead text-center">About Us</h1>
             <p>The diner is open from 6 am to 10 pm --and free parking is available behind our location at 320 Lancaster Ave, Wayne, Pennsylvania </p>
+            <p class="Lead text-success">Read More </p>
         </div>
         <div class="col">
-            <h1 class="lead text-center">our Mission</h1>
+            <h1 class="lead text-center text-center">Our Mission</h1>
             <p>Our restaurant strives to become our customers’ favorite place to eat and drink.</p>
+            <p class="Lead text-success text-center">Read More </p>
         </div>
         </div>
-    </div>
-    <div class="container">
+    </div><br><br>
+    <div class="container-fluid bg-light">
         
-        <div class="row ms-2 bg-color-light">
-            <div class="col-4">
-         <img src="{{  URL('storage/assets/resto1.jpg') }}"  width="70%" height="80%"  alt="image" id="firstImage" ></div>
-         <div class="col-8 ps-0" ><h1 class="lead text-center">About Us</h1>
-            <p>The diner is open from 6 am to 10 pm --and free parking is available behind our location at 320 Lancaster Ave, Wayne, Pennsylvania </p>
+        <div class="row ms-2 mt-5">
+            <div class="col-8">
+         <img src="{{  URL('storage/assets/cake.webp') }}"  width="100%" height="90%"  alt="image" id="firstImage" ></div>
+         <div class="col-4 ps-0" >
+            <p>Superior service, personalized attention
+                We're proud to offer the highest quality, freshest diner food on The Main Line. From our family to yours, we put lots of love and careful attention in each item. We hope you enjoy our work as much as we enjoy bringing it to you.
+                
+                A local favorite since 1971, Minella’s offers an extensive menu with everything imaginable at a diner, including salads, soups, soft drinks, desserts, breakfast foods, eggs, pancakes, waffles, hot and cold sandwiches, burgers, wraps, seafood, steak, plus Italian and Greek specialties.
+                
+                All baking is done on the premises, and credit cards are accepted. We also offer a full bar menu.
+                
+                It's our honor to serve you. Welcome and enjoy! </p>
         </div>
     </div>
   
 </body>
-C:\xampp\htdocs\Restaurant_Laravel_Project\restaurant_app\public\storage\assets\cake.webp
+
 </html>
 @stop
