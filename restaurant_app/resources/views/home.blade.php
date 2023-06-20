4a4d2e7a9e51b58f0f878bb68f978e7ec205dbac
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-20 13:26:11 +0300

    Changed image

--- restaurant_app/resources/views/home.blade.php
+++ restaurant_app/resources/views/home.blade.php
@@ -12,7 +12,7 @@
 <body style="background-color:rgba(197, 170, 170, 0.188)">
     <div class="container bg-color-primary">
         <div class="images">
-         <img src="{{  URL('storage/assets/resto1.jpg') }}"  width="100%" height="70%"  alt="image" id="firstImage" ><br><br>
+         <img src="{{  URL('storage/assets/resto11.jpg') }}"  width="100%" height="70%"  alt="image" id="firstImage" ><br><br>
          <img src="{{  URL('storage/assets/resto1.jpg') }}"  width="50%" height="40%"  alt="image" id="firstImage" >
         </div>
 
