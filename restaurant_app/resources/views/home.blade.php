d3ae0886b87973d4e17bf11a70e0015dd2b9e5f5
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-20 13:23:22 +0300

    Added an image

--- restaurant_app/resources/views/home.blade.php
+++ restaurant_app/resources/views/home.blade.php
@@ -12,8 +12,10 @@
 <body style="background-color:rgba(197, 170, 170, 0.188)">
     <div class="container bg-color-primary">
         <div class="images">
-         <img src="{{  URL('storage/assets/resto1.jpg') }}"  width="100%" height="70%"  alt="image" id="firstImage" >
+         <img src="{{  URL('storage/assets/resto1.jpg') }}"  width="100%" height="70%"  alt="image" id="firstImage" ><br><br>
+         <img src="{{  URL('storage/assets/resto1.jpg') }}"  width="50%" height="40%"  alt="image" id="firstImage" >
         </div>
+
     </div>
   
 </body>
