145f5bd77f84f29143d817967c707e9ee7bd08bf
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-20 15:16:36 +0300

    created a div tag with text mission

--- restaurant_app/resources/views/home.blade.php
+++ restaurant_app/resources/views/home.blade.php
@@ -12,10 +12,23 @@
 <body style="background-color:rgba(197, 170, 170, 0.188)">
     <div class="container bg-color-primary">
         <div class="images">
-         <img src="{{  URL('storage/assets/resto11.jpg') }}"  width="100%" height="70%"  alt="image" id="firstImage" ><br><br>
-         <img src="{{  URL('storage/assets/resto1.jpg') }}"  width="50%" height="40%"  alt="image" id="firstImage" >
+         <img src="{{  URL('storage/assets/resto11.jpg') }}"  width="100%" height="70%"  alt="image" id="firstImage" ></div><br><br>
         </div>
+         <div class="row ms-2 bg-color-light">
+            <div class="col-8">
+         <img src="{{  URL('storage/assets/resto1.jpg') }}"  width="70%" height="80%"  alt="image" id="firstImage" ></div>
+         <div class="col-3"><h1 class="lead text-center">About Us</h1>
+            <p>We are the BiWaMi Restaurant that provides best services to its clients</p>
+        </div>
+        </div>
+    </div>
+    <div class="container">
+        <h1 class="lead text-center">our Mission
 
+        </h1>
+        <div class="ms-5">
+         
+        </div>
     </div>
   
 </body>
