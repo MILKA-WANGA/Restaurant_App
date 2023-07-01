ff861e47516bfb304beb801659a56ea5b8250799
Milka Wanga <bakhuyamilka@gmail.com>
2023-07-01 18:30:54 +0300

    Edited class button in login.blade.php file

--- restaurant_app/resources/views/login.blade.php
+++ restaurant_app/resources/views/login.blade.php
@@ -20,12 +20,16 @@
                 <div class="form-group">
                 <label for="name">Pasword:</label>
                <input class="form-control" type="password" name="password" placeholder="mtkalo123"><br><br></div>
-    
-                <a href="/register" style="text-decoration:none;font-size:12px;">create account</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <a href="/password" style="text-decoration:none;font-size:12px;">Forgot password</a><br><br>
-                <button class="btn btn-secondary " style="width:70px;height:35px; text-size:4px" type="submit">Login</button>
-
-
+               <div class="form-group">
+               <button class="btn  btn-block btn-secondary " style="text-size:4px" type="submit">Login</button>
+               </div>
+               <div class="form-group">
+                <a  class="form-control"  href="/register" style="text-decoration:none;font-size:12px;">create account</a>
+               </div>
+               <div class="form-group">
+                <a  class="form-control"   href="/password" style="text-decoration:none;font-size:12px;">Forgot password</a><br><br>
+               </div>
+                
             </form>
         </center>
        
