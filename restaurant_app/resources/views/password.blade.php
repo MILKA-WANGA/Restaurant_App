08c9bbd9bb736d81205f663223e3c424c5edd878
Milka Wanga <bakhuyamilka@gmail.com>
2023-07-12 15:25:34 +0300

    Added a background-color to button

--- restaurant_app/resources/views/password.blade.php
+++ restaurant_app/resources/views/password.blade.php
@@ -14,11 +14,12 @@
             <h3 class="text-center text-muted text-lead">Enter your email</h3>
             <center>
             <form action="/password"  method="POST"  id="form1" class="p-2 m-2 bg-light "style="margin-bottom:20px;" >
+                  @csrf
                 <div class="form-group">
                     <label for="email">Email:</label>      
                     <input class="form-control" type="email" name="email" placeholder="example@gmail.com"><br><br></div>
                 <div class="form-group">
-                     <button class="btn  btn-block btn-secondary " style="text-size:4px" type="submit">Submit</button>
+                     <button class="btn  btn-block btn-primary " style="text-size:4px" type="submit">Reset Password Link</button>
                 </div>
                
                 <a href="/login" style="text-decoration:none;font-size:12px;">Login</a><br><br>
